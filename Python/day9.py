fd0fef60bb15b71ab90aad10f96535a45fc61257
jzfraser <jackzfras@gmail.com>
2023-07-06 14:15:44 -0600

    add day 9

---
+++ Python/day9.py
@@ -0,0 +1,25 @@
+from sys import maxsize
+from itertools import permutations
+
+f = open("day9input.txt")
+paths = f.readlines()
+
+graph = {}
+
+for path in paths:
+    parts = path.strip().split(" ")
+    start = parts[0]
+    end = parts[2]
+    dist = int(parts[-1])
+    graph.setdefault(start, {})[end] = dist
+    graph.setdefault(end, {})[start] = dist
+
+shortest = maxsize
+longest = 0
+for perms in permutations(graph):
+    dist = sum(map(lambda i, j: graph[i][j], perms[:-1], perms[1:]))
+    shortest = min(shortest, dist)
+    longest = max(longest, dist)
+
+print(shortest)
+print(longest)
